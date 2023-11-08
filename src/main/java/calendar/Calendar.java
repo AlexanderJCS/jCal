cf89b2818ac6230a3869373a5a19d37bda3bef26
Alex <alexandercastronovo@gmail.com>
2023-11-07 19:10:21 -0500

    Events have random colors

--- src/main/java/calendar/Calendar.java
+++ src/main/java/calendar/Calendar.java
@@ -1,6 +1,10 @@
 package calendar;
 
+import jangl.color.Color;
+import jangl.color.ColorFactory;
 import jangl.coords.WorldCoords;
+import jangl.graphics.shaders.ShaderProgram;
+import jangl.graphics.shaders.premade.ColorShader;
 
 import java.time.LocalTime;
 import java.util.ArrayList;
@@ -9,6 +13,7 @@ import java.util.List;
 public class Calendar implements AutoCloseable {
     private final CalendarCanvas canvas;
     private final List<CalendarEvent> events;
+    private final ShaderProgram colorShader;
 
     public Calendar() {
         this.canvas = new CalendarCanvas(
@@ -17,6 +22,8 @@ public class Calendar implements AutoCloseable {
                 LocalTime.of(8, 0),
                 LocalTime.of(20,0)
         );
+
+        this.colorShader = new ShaderProgram(new ColorShader(ColorFactory.fromNormalizedHSVA((float) Math.random(), 0.7f, 0.7f, 1)));
 
         this.events = new ArrayList<>();
         this.events.add(new CalendarEvent("Test", this.canvas, WeekDay.FRI, LocalTime.of(9, 0), LocalTime.of(17, 0)));
@@ -28,9 +35,15 @@ public class Calendar implements AutoCloseable {
         }
     }
 
+    public void addEvent(CalendarEvent event) {
+        this.events.add(event);
+    }
+
     public void draw() {
         for (CalendarEvent event : this.events) {
+            this.colorShader.bind();
             event.draw();
+            this.colorShader.unbind();
         }
     }
 
