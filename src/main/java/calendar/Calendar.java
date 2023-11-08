ced3c3ee8a87f9465d2906397b6816ee6d872ce0
Alex <alexandercastronovo@gmail.com>
2023-11-07 19:02:50 -0500

    Make Calendar.setCalendarNumber functional

--- src/main/java/calendar/Calendar.java
+++ src/main/java/calendar/Calendar.java
@@ -8,7 +8,7 @@ import java.util.List;
 
 public class Calendar implements AutoCloseable {
     private final CalendarCanvas canvas;
-    private final List<CalendarEvent> items;
+    private final List<CalendarEvent> events;
 
     public Calendar() {
         this.canvas = new CalendarCanvas(
@@ -18,24 +18,26 @@ public class Calendar implements AutoCloseable {
                 LocalTime.of(20,0)
         );
 
-        this.items = new ArrayList<>();
-        this.items.add(new CalendarEvent("Test", this.canvas, WeekDay.FRI, LocalTime.of(9, 0), LocalTime.of(17, 0)));
+        this.events = new ArrayList<>();
+        this.events.add(new CalendarEvent("Test", this.canvas, WeekDay.FRI, LocalTime.of(9, 0), LocalTime.of(17, 0)));
     }
 
     public void setCalendarNumber(int calendarNumber, int numCalendars) {
-
+        for (CalendarEvent event : this.events) {
+            event.setCalendarNumber(calendarNumber, numCalendars);
+        }
     }
 
     public void draw() {
-        for (CalendarEvent item : this.items) {
-            item.draw();
+        for (CalendarEvent event : this.events) {
+            event.draw();
         }
     }
 
     @Override
     public void close() {
-        for (CalendarEvent item : this.items) {
-            item.close();
+        for (CalendarEvent event : this.events) {
+            event.close();
         }
     }
 }
